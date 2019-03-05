f158e406f1c4a110ddb819f4e8f850c91444d3c0
Stine Scheuer <stine.scheuer@regionh.dk>
2019-03-05 11:33:16 +0100

    head iris

---
+++ R/version-control-session.R
@@ -0,0 +1,14 @@
+head(iris)
+summary(iris)
+
+sepal_length <- iris["Sepal.Length"]
+sepal_length
+
+head(iris)
+summary(iris)
+
+sepal_length <- iris["Sepal.Length"]
+sepal_length
+
+head(iris)
+summary(iris)
